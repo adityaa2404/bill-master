af3aa3155b85463d80ea8469b6ebc693f8200b11
adityaa2404 <studious1000@gmail.com>
2025-11-27 12:52:52 +0530

    minor fixes

--- frontend/config/api.js
+++ frontend/config/api.js
@@ -1,9 +1,11 @@
+// frontend/src/config/api.js
 import axios from "axios";
 
-export const API_BASE = "http://localhost:8000/api";
-
+const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:8000";
+console.log("🔥 FRONTEND API BASE URL:", API_BASE_URL);
 const api = axios.create({
-  baseURL: API_BASE,
+  baseURL: `${API_BASE_URL}/api`,
 });
 
 export default api;
+
